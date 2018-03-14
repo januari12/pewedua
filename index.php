572a8a0ab175f4a35568a5b7d4002aa37673296c
januari12 <januari.yanti@student.untan.ac.id>
2018-03-14 13:04:03 +0700

    Latihan 1,halaman perdana

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+//ini adalah komentar saja
+// bagaimana bila
+// engkau wahai komentar
+// berpanjang lebar dalam berkata
+	echo "Januari Yanti";
+	echo "H1101161023";
+
+	echo "Hello world!";
+	echo "Hello world!";
+	
+
+?>
